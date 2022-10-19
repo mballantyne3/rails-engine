a210283d3f915693054cde33518e909ffc0cfbee
Mary Ballantyne <mballantyne777@gmail.com>
2022-10-19 09:28:25 -0600

    feature added to delete an item

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show, :create, :update] do
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], controller: :merchant_items
       end
-      resources :items, only: [:index, :show, :create, :update]
+      resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe "Items API" do
       name: 'Deathwish Blend',
       description: 'this will violently wake you up',
       unit_price: 24.50,
-      merchant_id: merchant.id}
-    headers = { "CONTENT_TYPE" => "application/json"}
+      merchant_id: merchant.id }
+    headers = { "CONTENT_TYPE" => "application/json" }
 
     post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
     created_item = Item.last
@@ -64,14 +64,26 @@ RSpec.describe "Items API" do
       name: 'Get Outta Bed Blend',
       description: 'this will softly wake you up',
       unit_price: 25.50,
-      merchant_id: merchant.id}
-    headers = { "CONTENT_TYPE" => "application/json"}
+      merchant_id: merchant.id }
+    headers = { "CONTENT_TYPE" => "application/json" }
 
-    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({ item: item_params })
     item = Item.find_by(id: id)
 
     expect(response).to be_successful
     expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq("Get Outta Bed Blend")
+  end
+
+  it 'can delete an existing item' do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect { Item.find(item.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
