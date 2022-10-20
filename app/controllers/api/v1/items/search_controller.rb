60bdaee5476a7feb2b412f6c6e0d564b2fc0cbcf
Mary Ballantyne <mballantyne777@gmail.com>
2022-10-20 16:01:16 -0600

    added min and max price functionality to the search items method with spec

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,15 +1,34 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def find_one
-    item = Item
-      .where('name ILIKE ?', "%#{params[:name]}%") #todo: .downcase
-      .order(Arel.sql("LOWER(name)"))
-      .first
+    # Parameter parsing
+    name_search = params[:name]
+    min_price = params[:min_price]&.to_i
+    max_price = params[:max_price]&.to_i
 
+    # Validation
+    if name_search.present? && (min_price.present? || max_price.present?)
+      render json: { error: 'that would be too many params dude' }, status: 400 and return
+    elsif min_price.present? && min_price < 0
+      render json: { error: 'uhhhh... you think we are giving away items for free?' }, status: 400 and return
+    elsif max_price.present? && max_price < 0
+      render json: { error: 'nope, still not giving things away for free' }, status: 400 and return
+    end
+
+    # Query building
+    possible_items = Item.order Arel.sql("LOWER(name)")
+    possible_items.where!('name ILIKE ?', "%#{name_search}%") if name_search
+    possible_items.where!('unit_price >= ?', min_price) if min_price
+    possible_items.where!('unit_price <= ?', max_price) if max_price
+
+    # Query execution
+    item = possible_items.first
+
+    # Response
     if item
       render json: ItemSerializer.new(item)
     else
-      render json: { data: {message: 'No item matches your search'}}, status: 400
+      render json: { data: { message: 'No item matches your search' } }, status: 400
     end
   end
 end
--- spec/requests/api/v1/items/items_find_spec.rb
+++ spec/requests/api/v1/items/items_find_spec.rb
@@ -38,6 +38,87 @@ RSpec.describe 'Find Items API' do
       get "/api/v1/items/find?name=light"
 
       expect(response.successful?).to eq false
+      expect(response).to have_http_status(400)
+    end
+  end
+
+  describe 'price related search queries' do
+    it 'can search for the first item whose price is greater than or equal to the minimum price point' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 3.45, merchant_id: merchant.id)
+      item2 = Item.create!(name: "Saurons Dark Blend", description: 'darker than its predecessor', unit_price: 7.89, merchant_id: merchant.id)
+      item3 = Item.create!(name: "Dark as my Soul Blend", description: 'darker STILL', unit_price: 5.99, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?min_price=5"
+
+      expect(response).to be_successful
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:data][:id].to_i).to eq item3.id
+    end
+    it 'can search for the first item whose price is less than or equal to the maximum price point' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 15.05, merchant_id: merchant.id)
+      item2 = Item.create!(name: "Saurons Dark Blend", description: 'darker than its predecessor', unit_price: 20, merchant_id: merchant.id)
+      item3 = Item.create!(name: "Dark as my Soul Blend", description: 'darker STILL', unit_price: 17.99, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?max_price=18"
+
+      expect(response).to be_successful
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:data][:id].to_i).to eq item3.id
+    end
+
+    it 'can allow the user to send one or more price related queries' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 15.05, merchant_id: merchant.id)
+      item2 = Item.create!(name: "Saurons Dark Blend", description: 'darker than its predecessor', unit_price: 20, merchant_id: merchant.id)
+      item3 = Item.create!(name: "Dark as my Soul Blend", description: 'darker STILL', unit_price: 18, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?max_price=17&min_price=15"
+
+      expect(response).to be_successful
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      expect(response_body[:data][:id].to_i).to eq item1.id
+    end
+  end
+
+  describe 'price parameter sad paths' do
+    it 'will return an error if name param is sent with either price parameters' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 15.05, merchant_id: merchant.id)
+      item2 = Item.create!(name: "Saurons Dark Blend", description: 'darker than its predecessor', unit_price: 20, merchant_id: merchant.id)
+      item3 = Item.create!(name: "Dark as my Soul Blend", description: 'darker STILL', unit_price: 18, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?name=dark&min_price=15"
+
+      expect(response.successful?).to eq false
+      expect(response).to have_http_status 400
+
+      get "/api/v1/items/find?name=dark&max_price=15"
+
+      expect(response.successful?).to eq false
+      expect(response).to have_http_status 400
+    end
+
+    it 'will return an error if min_price is entered as less than zero' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 15.05, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?min_price=-15"
+
+      expect(response.successful?).to eq false
+      expect(response).to have_http_status 400
+    end
+
+    it 'will return an error if max_price is entered as less than zero' do
+      merchant = create(:merchant)
+      item1 = Item.create!(name: "Dark Soul Blend", description: 'rich, chocolatey, soul crushing', unit_price: 15.05, merchant_id: merchant.id)
+
+      get "/api/v1/items/find?max_price=-18"
+
+      expect(response.successful?).to eq false
+      expect(response).to have_http_status 400
     end
   end
 end
